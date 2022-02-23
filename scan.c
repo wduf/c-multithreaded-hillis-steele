f62d96404e65173af6d1b1fe15fa6343917a2409
wduf <99445180+wduf@users.noreply.github.com>
2022-02-23 13:51:37 -0500

    Update scan.c

--- scan.c
+++ scan.c
@@ -1,151 +1,98 @@
-#include <assert.h>
-#include <stdio.h>
-#include <math.h>
-#include <pthread.h>
-#include <semaphore.h>
+// includes:
+
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
 
-//////
+// globals:
 
-#define MAX_LINE_SIZE 256
+int* input_g;  // global input array
+int* sums_g;  // global sum array
+int pos_g;  // global current position in array
 
-//////
+// functions:
 
-typedef struct __lock_t
+void* threadFunction(void* par)
 {
-	int ticket;
-	int turn;
-} lock_t;
+	int tn = *((long*) par);
+	int sum = 0;
 
-//////
-
-lock_t thread_lock;
-
-//////
-
-// starter
-void read_input_vector(const char* filename, int n, int* array)
-{
-	FILE* fp;
-	char* line = malloc(MAX_LINE_SIZE + 1);
-	size_t len = MAX_LINE_SIZE;
-	ssize_t read;
-	int index = 0;
-
-	fp = (strcmp(filename, "-") ? fopen(filename, "r") : stdin);
-
-	assert((fp != NULL) && (line != NULL));
-
-	while((read = getline(&line, &len, fp)) != -1)
-	{
-		array[index] = atoi(line);
-		index++;
+	for(int i = 0; i < (pos_g + tn); i++)
+	{  // calculate prefix sum
+		sum += input_g[i];
 	}
+	sums_g[pos_g + tn - 1] = sum;  // tn starts at 1
 
-	free(line);
-	fclose(fp);
+        return 0;
 }
 
-// slides 9
-int FetchAndAdd(int* ptr)
-{  // returns value at ptr, then increments it
-	int old = *ptr;
-	*ptr = old + 1;
-	return old;
-}
-
-// slides 9
-void lock_init(lock_t* thread_lock)
-{  // initializes a spinlock
-	thread_lock->ticket = 0;
-	thread_lock->turn = 0;
-}
-
-// slides 9
-void lock(lock_t* thread_lock)
-{  // locks a lock
-	int myturn = FetchAndAdd(&thread_lock->ticket);
-	while (thread_lock->turn != myturn);  //spin
-}
-
-// slides 9
-void unlock(lock_t* thread_lock)
-{  // unlocks a lock
-	thread_lock->turn += 1;
+void printSums(int loops)
+{  // print out prefix sums for (loops) sums
+	for(int i = 0; i < loops; i++)
+	{  // print out all values
+		printf("%d\n", sums_g[i]);
+	}
 }
 
 /*
-	WORK IN PROGRESS
-	- needs to contain locks
-	
-	NEED TO CHANGE UP ENTIRE SYSTEM:
-	PTHREAD_CREATE TAKES (VOID*) FCN(VOID*)
+        input = input array
+        size = size of input array
+        nt = number of threads
 */
-int carlBestBrawler(pthread_t* threads, int thread_number, int position, int size)  // position is starting position in array
-{  // calculates and prints prefix sums
-	int R = 1;  // default more values in array, set to 0 once end of array is reached
-	int sum = 0;  // accumulator for prefix sum
-
-	if((position + thread_number) > size)
-	{  // if all prefix sums calculated
-		R = 0;  // end of array reached
-	} else
-	{  // still prefix sums to be calculated
-		for(int i = 0; i < thread_number; i++)
-		{
-			sum += threads[i];
-		}
-		printf("%d\n", sum);
-	}
+void production(int* input, int size, int nt)
+{
+        pthread_t threads[size];  // place to store threads
+	int tn[size];  // place to store thread numbers (to pass into pthread_create)
+	int sums[size];
+	int end = 0;  // if end of input reached
+	input_g = input;  // make input global
+	sums_g = sums;  // make sums global
+	pos_g = 0;
 	
-	return R;
+	if(nt >= size)
+	{  // if we don't need to reuse threads
+		for(int i = 0; i < size; i++)
+		{  // create (nt) threads
+			tn[i] = (i + 1);
+			pthread_create(&threads[i], NULL, threadFunction, (void*) &tn[i]);  // (i + 1) is the thread number (in order of creation)
+		}
+		for(int i = 0; i < size; i++)
+		{  // don't stop until all threads are done executing
+			pthread_join(threads[i], NULL);
+		}
+	} else
+	{  // if we need to reuse threads
+		while(!end)
+		{
+			for(int i = 0; i < nt; i++)
+			{  // create (nt) threads
+				tn[i] = (i + 1);
+				if((pos_g + tn[i]) > size)
+				{
+					end = 1;  // reached end of array
+					break;
+				}
+				pthread_create(&threads[i], NULL, threadFunction, (void*) &tn[i]);  // (i + 1) is the thread number (in order of creation)
+			}
+			for(int i = 0; i < nt; i++)
+			{  // don't stop until all threads are done executing
+				pthread_join(threads[i], NULL);
+			}
+			pos_g += nt;  // start 1 after the last thread, reuse threads
+		}
+	}
+
+	printSums(size);
 }
 
-// slides 9
-int main(int argc, char* argv[])
+int main()
 {
-	char* filename = argv[1];
-	int size = atoi(argv[2]);  // size of input vector
-	int thread_count = atoi(argv[3]);  // number of threads
+        int input[4] = {1, 2, 3, 4};
+        int size = 4;
+        int nt = 1;
 
-	if(size < 2)
-		exit(EXIT_FAILURE);
-	int* input= malloc(size * sizeof(int));
-	read_input_vector(filename, size, input);
-	
-	// end of starter code
-	
-	pthread_t threads[thread_count];  // array of threads, stack faster than heap
-	int position = 0;  // position in array accumulator
-	int keep_going;  // whether or not to do another loop of carlBestBrawler
-		
-	if(thread_count >= size)
-	{  // if there are enough threads to go through entire vector without needing to reuse threads
-		for(int i = 0; i < size; i++)
-		{
-			// i is the thread number
-			keep_going = pthread_create(&threads[i], NULL, carlBestBrawler, threads, i, 0, size);
-			// might need to implement a pthread_join(&threads[i]) here to make sure they're running in order, but this is slow
-		}	
-	} else
-	{  // we need to reuse threads, need a barrier
-		while(keep_going)
-		{
-			for(int i = 0; i < thread_count; i++)
-			{
-				keep_going = pthread_create(&threads[i], NULL, carlBestBrawler, threads, i, position, size);
-				// might need to implement a pthread_join(&threads[i]) here to make sure they're running in order, but this is slow
-				if(!keep_going)
-				{
-					break;  // reached end of array
-				}
-			}
-			position += thread_count;  // next loop
-		}
-	}
-	
-	free(input);
+        production(input, size, nt);
 
-	return 0;
+        return 0;
 }
