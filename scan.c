b4f2374bb5beb5cd89c073a0b3611fa5772332ab
wduf <99445180+wduf@users.noreply.github.com>
2022-02-23 22:17:51 -0500

    Update scan.c

--- scan.c
+++ scan.c
@@ -1,102 +1,184 @@
-// includes:
+// PICTURE OF ME
+
+/*⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠛⠛⠛⠋⠉⠈⠉⠉⠉⠉⠛⠻⢿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⡿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⢿⣿⣿⣿⣿
+⣿⣿⣿⣿⡏⣀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿
+⣿⣿⣿⢏⣴⣿⣷⠀⠀⠀⠀⠀⢾⣿⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿
+⣿⣿⣟⣾⣿⡟⠁⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣷⢢⠀⠀⠀⠀⠀⠀⠀⢸⣿
+⣿⣿⣿⣿⣟⠀⡴⠄⠀⠀⠀⠀⠀⠀⠙⠻⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⣿
+⣿⣿⣿⠟⠻⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠶⢴⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⣿
+⣿⣁⡀⠀⠀⢰⢠⣦⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣿⡄⠀⣴⣶⣿⡄⣿
+⣿⡋⠀⠀⠀⠎⢸⣿⡆⠀⠀⠀⠀⠀⠀⣴⣿⣿⣿⣿⣿⣿⣿⠗⢘⣿⣟⠛⠿⣼
+⣿⣿⠋⢀⡌⢰⣿⡿⢿⡀⠀⠀⠀⠀⠀⠙⠿⣿⣿⣿⣿⣿⡇⠀⢸⣿⣿⣧⢀⣼
+⣿⣿⣷⢻⠄⠘⠛⠋⠛⠃⠀⠀⠀⠀⠀⢿⣧⠈⠉⠙⠛⠋⠀⠀⠀⣿⣿⣿⣿⣿
+⣿⣿⣧⠀⠈⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠟⠀⠀⠀⠀⢀⢃⠀⠀⢸⣿⣿⣿⣿
+⣿⣿⡿⠀⠴⢗⣠⣤⣴⡶⠶⠖⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡸⠀⣿⣿⣿⣿
+⣿⣿⣿⡀⢠⣾⣿⠏⠀⠠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠉⠀⣿⣿⣿⣿
+⣿⣿⣿⣧⠈⢹⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿
+⣿⣿⣿⣿⡄⠈⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣾⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣦⣄⣀⣀⣀⣀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠙⣿⣿⡟⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠁⠀⠀⠹⣿⠃⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⢐⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⠿⠛⠉⠉⠁⠀⢻⣿⡇⠀⠀⠀⠀⠀⠀⢀⠈⣿⣿⡿⠉⠛⠛⠛⠉⠉
+⣿⡿⠋⠁⠀⠀⢀⣀⣠⡴⣸⣿⣇⡄⠀⠀⠀⠀⢀⡿⠄⠙⠛⠀⣀⣠⣤⣤⠄⠀
+*/
+
+// INCLUDES:
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <pthread.h>
+#include <math.h>
+#include <string.h>
+#include <unistd.h>
 
-// globals:
+// GLOBALS:
 
-int* input_g;  // global input array
-int* sums_g;  // global sum array
-int pos_g;  // global current position in array
+pthread_mutex_t mutex_lock;  // mutually exclusive lock for threads
+int* ready_g;  // global ready states for threads
+int* input_g; // global input array
+int* sums_g;  // global prefix sum array
+int* mid_sums_g;  // global sum in between steps array
+int size_g;  // global size of input
+int nt_g;  // global number of threads
 
-// functions:
+// FUNCTIONS:
 
-void* threadFunction(void* par)
-{
-	int tn = *((long*) par);
-	int sum = 0;
+int calculateLoops(int tn, int jump)
+{  // calculate how many loops thread (tn) needs to take this step
+	int R = 0;  // return value
+	int jumps_left = (size_g - jump);
 
-	for(int i = 0; i < (pos_g + tn); i++)
-	{  // calculate prefix sum
-		sum += input_g[i];
+	while(jumps_left > nt_g)
+	{
+		{  // figure out how many loops this thread needs to take
+			R++;
+			jumps_left -= nt_g;
+		}
 	}
-	sums_g[pos_g + tn - 1] = sum;  // tn starts at 1
+	if(jumps_left >= tn)
+	{
+		R++;
+	}
 
-        return 0;
+	return R;
 }
 
-void printSums(int loops)
-{  // print out prefix sums for (loops) sums
-	for(int i = 0; i < loops; i++)
-	{  // print out all values
+void printSums()
+{
+	for(int i = 0; i < size_g; i++)
+	{
 		printf("%d\n", sums_g[i]);
 	}
 }
 
+int allThreadsReady()
+{
+	int R = 1;  // return value
+	
+	for(int i = 0; i < nt_g; i++)
+	{
+		if(!ready_g[i])
+		{  // if a thread is not ready
+			R = 0;
+			break;
+		}
+	}
+
+	return R;
+}
+
+void* threadFunction(void* par)
+{
+	int tn = *((long*) par);  // thread number
+	int step = 0;  // step/row counter
+	int jump = 1;  // how many numbers to jump forward to calculate current sum, start at 1 because 2^0 = 1
+	int loops = 0;  // total loops this thread needs to perform this step
+
+	while(jump < size_g)
+	{  // while there is another sum to be calculated
+		printf("new while in thread%d\n", tn);
+		loops = calculateLoops(tn, jump);
+		for(int i = 0; i < loops; i++)
+		{  // add input values
+			int index = (jump + (tn - 1) + (i * nt_g));
+			printf("thread%d: in first for: sums[%d] = (input[%d]) %d + (input[%d]) %d\n", tn, index, index, input_g[index], (index - jump), input_g[index - jump]);
+			mid_sums_g[index] = (input_g[index] + input_g[index - jump]);
+		}
+		ready_g[tn - 1] = 1;
+		printf("thread%d waiting...\n", tn);
+		while(!allThreadsReady()); // barrier
+		printf("moving forward\n");
+		ready_g[tn - 1] = 0;
+		memcpy(input_g, mid_sums_g, sizeof(mid_sums_g));
+		step++;	
+		jump = pow(2, step);
+		//printf("thread%d: moving to jump = %d\n", tn, jump); fflush(stdout);
+	}
+	for(int i = 0; i < jump; i++)
+	{  // set values in prefix sum array
+		if(sums_g[i] != input_g[i])
+		{
+			//printf("thread%d sets sums[%d] to %d\n", tn, i, input_g[i]);
+			sums_g[i] = input_g[i];
+		}
+	}
+	
+	return 0;
+}
+
 /*
-        input = input array
-        size = size of input array
-        nt = number of threads
+	input = int array
+	size = size of input
+	nt = number of threads
 */
 void production(int* input, int size, int nt)
 {
-        pthread_t threads[size];  // place to store threads
-	int tn[size];  // place to store thread numbers (to pass into pthread_create)
-	int sums[size];
-	int end = 0;  // if end of input reached
-	int curr_threads = 0;  // counter for current threads running
-	input_g = input;  // make input global
-	sums_g = sums;  // make sums global
-	pos_g = 0;
-	
-	if(nt >= size)
-	{  // if we don't need to reuse threads
-		for(int i = 0; i < size; i++)
-		{  // create (nt) threads
-			tn[i] = (i + 1);
-			pthread_create(&threads[i], NULL, threadFunction, (void*) &tn[i]);  // (i + 1) is the thread number (in order of creation)
-			curr_threads++;
-		}
-		for(int i = 0; i < curr_threads; i++)
-		{  // don't stop until all threads are done executing
-			pthread_join(threads[i], NULL);
-			curr_threads--;
-		}
-	} else
-	{  // if we need to reuse threads
-		while(!end)
-		{
-			for(int i = 0; i < nt; i++)
-			{  // create (nt) threads
-				tn[i] = (i + 1);
-				if((pos_g + tn[i]) > size)
-				{
-					end = 1;  // reached end of array
-					break;
-				}
-				pthread_create(&threads[i], NULL, threadFunction, (void*) &tn[i]);  // (i + 1) is the thread number (in order of creation)
-				curr_threads++;
-			}
-			for(int i = 0; i < curr_threads; i++)
-			{  // don't stop until all threads are done executing
-				pthread_join(threads[i], NULL);
-				curr_threads--;
-			}
-			pos_g += nt;  // start 1 after the last thread, reuse threads
-		}
+	pthread_t threads[nt];  // array to store thread
+	int tn[nt];
+
+	for(int i = 0; i < nt; i++)
+	{  // create (nt) threads
+		tn[i] = (i + 1);
+		pthread_create(&threads[i], NULL, threadFunction, (void*) &tn[i]);
+		printf("thread%d created\n", tn[i]);
 	}
-	printSums(size);
+	for(int i = 0; i < nt; i++)
+	{  // wait for all threads to finish executing
+		pthread_join(threads[i], NULL);
+	}
+	printSums();
+
+	return;
 }
 
 int main()
 {
-        int input[32] = {793, 283, 735, 755, 637, 266, 826, 175, 107, 651, 466, 949, 754, 60, 52, 948, 3, 263, 592, 511, 316, 784, 45, 139, 398, 545, 976, 394, 38, 403, 689, 182};
-        int size = 32;
-        int nt = 4;
+	int input[4] = {1, 2, 3, 4};  // input array
+	int size = 4;  // size of input array
+	int nt = 2;  // number of threads
+	int sums[size];  // array of prefix sums
+	int ready[nt];  // array of booleans for if each thread is ready to move on
 
-        production(input, size, nt);
+	ready_g = ready;  // make ready global
+	input_g = input;  // make input global
+	sums_g = sums;  // make sums global
+	mid_sums_g = input;  // make mid_sums global
+	memcpy(mid_sums_g, input, sizeof(input));
+	size_g = size;  // make size global
+	nt_g = nt;  // make nt global
 
-        return 0;
+	for(int i = 0; i < nt; i++)
+	{  // initialize ready_g
+		ready_g[i] = 0;  // by default, threads aren't ready to move on yet
+	}
+
+	production(input, size, nt);
+
+	return 0;
 }
