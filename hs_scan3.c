f966de90ade6d750b470ce346ce7c10dcc68c0ec
wduf <99445180+wduf@users.noreply.github.com>
2022-02-25 13:18:09 -0500

    Update hs_scan3.c

--- hs_scan3.c
+++ hs_scan3.c
@@ -18,7 +18,7 @@ typedef struct __args_t
 	int* n_t;  // number of threads
 	int t_n;  // thread number
 	int* t_r;  // counter for ready threads
-	pthread_mutex_t* lock;  // mutually exclusive lock for threads
+	int* t_r2;  // for second barrier
 } args_t;
 
 // GLOBALS:
@@ -35,7 +35,7 @@ void printSums(int* sums, int size)
 	}
 }
 
-void initArgs(args_t* args, int* input, int* mids, int* sums, int* size, int* n_t, int t_n, int* t_r)
+void initArgs(args_t* args, int* input, int* mids, int* sums, int* size, int* n_t, int t_n, int* t_r, int* t_r2)
 {
 	args->input = input;
 	args->mids = mids;
@@ -44,6 +44,7 @@ void initArgs(args_t* args, int* input, int* mids, int* sums, int* size, int* n_
 	args->n_t = n_t;
 	args->t_n = t_n;
 	args->t_r = t_r;
+	args->t_r2 = t_r2;
 }
 
 void* threadFunction(void* arg)
@@ -64,7 +65,8 @@ void* threadFunction(void* arg)
 		while(jumps_left >= *args.n_t)
 		{  // if this thread is responsible for another jump
 			args.mids[index] = (args.input[index] + args.input[index - jump]);
-			printf("%d: setting mid[%d] = %d\n", args.t_n, index, args.mids[index]); fflush(stdout);
+			printf("%d: setting mid[%d] = %d, ", args.t_n, index, args.mids[index]); fflush(stdout);
+			printf("input[%d] = %d, input[%d] = %d\n", index, args.mids[index], index - jump, args.mids[index - jump]);
 			jumps_left -= *args.n_t;
 			printf("%d: now jumps left = %d\n", args.t_n, jumps_left); fflush(stdout);
 			index -= *args.n_t;
@@ -76,8 +78,8 @@ void* threadFunction(void* arg)
 		{  // edge case
 			printf("%d: %d >= %d\n", args.t_n, jumps_left, args.t_n);
 			printf("%d: mids[%d] = index[%d] + index[%d]\n", args.t_n, index, index, index - jump);
-			args.mids[index] = (args.input[index] + args.input[index - jump]);fflush(stdout);
-			printf("%d: setting mid[%d] = %d\n", args.t_n, index - jump, args.mids[index - jump]); fflush(stdout);
+			args.mids[index] = (args.input[index] + args.input[index - jump]);
+			printf("%d: setting mid[%d] = %d\n", args.t_n, index, args.mids[index]); fflush(stdout);
 		}
 		pthread_mutex_lock(&lock);
 		//printf("%d: locked, before t_r = %d\n", args.t_n, *args.t_r); fflush(stdout);
@@ -88,7 +90,11 @@ void* threadFunction(void* arg)
 		//printf("%d: before, input[0] = %d\n", args.t_n, args.input[0]); fflush(stdout);
 		memcpy(args.input, args.mids, (*args.size * sizeof(int)));
 		//printf("-> %d: moving on\n", args.t_n);
-
+		pthread_mutex_lock(&lock);
+		//printf("%d: locked, before t_r = %d\n", args.t_n, *args.t_r); fflush(stdout);
+		*args.t_r2 += 1;
+		pthread_mutex_unlock(&lock);
+		while(*args.t_r2 < (step * *args.n_t));
 		step++;
 		jump = (1 << (step - 1));
 	}
@@ -104,7 +110,7 @@ void* threadFunction(void* arg)
 	return 0;
 }
 
-void production(int* input, int* mids, int* sums, int* size, int* n_t, int* t_r)
+void production(int* input, int* mids, int* sums, int* size, int* n_t, int* t_r, int* t_r2)
 {
 	pthread_t* threads = (pthread_t*) malloc(*n_t * sizeof(pthread_t));  // thread array
 	args_t args[*size];  // struct array
@@ -118,7 +124,7 @@ void production(int* input, int* mids, int* sums, int* size, int* n_t, int* t_r)
 	//printf("got here\n"); fflush(stdout);
 	for(int i = 0; i < *n_t; i++)
 	{  // create threads
-		initArgs(&args[i], input, mids, sums, size, n_t, (i + 1), t_r);
+		initArgs(&args[i], input, mids, sums, size, n_t, (i + 1), t_r, t_r2);
 		pthread_create(&threads[i], NULL, threadFunction, (void*) &args[i]);
 	}
 	//printf("got here\n"); fflush(stdout);
@@ -134,7 +140,7 @@ int main()
 {
 	int input[32] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};  // input array
 	int size = 16;  // size of input
-	int n_t = 4;  // number of threads
+	int n_t = 8;  // number of threads
 	int* mids = (int*) malloc(size * sizeof(int));
 	int* sums = (int*) malloc(size * sizeof(int));
 	for(int i = 0; i < size; i++)
@@ -144,8 +150,9 @@ int main()
 	}
 	memcpy(mids, input, (size * sizeof(int)));
 	int t_r = 0;  // counter for threads ready
+	int t_r2 = 0;  // counter for second barrier
 
-	production(input, mids, sums, &size, &n_t, &t_r);
+	production(input, mids, sums, &size, &n_t, &t_r, &t_r2);
 
 	return 0;
 }
